cefe3f76ae4e29e627f2f368dedaa8f3bc3ce4b0
codaersimon <codaersimon@gmail.com>
2021-08-12 14:35:31 +0200

    Lev1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,47 @@
-console.log("test")
+console.log("----Lev1_3----")
+
+let i = 1;  
+console.log(typeof(i))   
+
+let lastName = "Johnson";
+console.log(typeof(lastName)) 
+
+let j = "2";
+console.log(typeof(j)) 
+
+let status = true;
+console.log(typeof(status)) 
+
+let hello = "hello";
+console.log(typeof(hello)) 
+
+let helloWorld = "hello";
+console.log(typeof(helloWorld)) 
+
+console.log("----Lev1_4----")
+// "John"                
+// 3.14                   
+// NaN                    
+// false                  
+// [1,2,3,4]              
+// {name:'John', age:34}  
+// new Date()             
+// function () {}         
+// null
+// ""
+// 3+2==5 
+// 3 + "3"
+
+console.log(typeof("John")) 
+console.log(typeof(3.14))
+console.log(typeof(NaN))
+console.log(typeof(false))
+console.log(typeof([1,2,3,4]))
+console.log(typeof({name:'John', age:34} ))
+console.log(typeof(function () {} ))
+console.log(typeof(null))
+console.log(typeof(""))
+console.log(typeof(3+2==5))
+console.log(typeof(3 + "3"))
+
+console.log("----Lev1_8-----")
